fbee7c118ab353ee649af453f1aed92125a959f0
ga-devfront <root@GameComputer.localdomain>
2020-09-26 11:02:21 +0200

    ✨ add SelectLocale component

---
+++ src/components/SelectLocale.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-select
+    rounded
+    dense
+    color="red"
+    v-model="$i18n.locale"
+    :items="locales"
+    prepend-icon="mdi-earth"
+    hide-details
+  >
+  </v-select>
+</template>
+
+<script>
+export default {
+  name: 'SelectLocal',
+
+  data() {
+    return {
+      locales: [
+        { value: 'fr', text: 'Français' },
+        { value: 'en', text: 'English' },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
